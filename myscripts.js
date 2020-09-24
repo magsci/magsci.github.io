0ecebcb5aff80cf9f77fe6f2d6ee6e57edaa78a5
magsci <stallardmargaret@btinternet.com>
2020-09-24 12:19:40 +0100

    myscripts.js

---
+++ myscripts.js
@@ -0,0 +1,7 @@
+function myShowMore(){ 	 
+	document.getElementById("end").style.display="block"; 
+}
+function myShowLess(){ 
+	document.getElementById("end").style.display="none"; 
+}
+document.addEventListener("DOMContentLoaded",init,false);
