59dd44a672cdd2f9db3474cf61808d39295eadcc
magsci <70955793+magsci@users.noreply.github.com>
2022-05-07 12:41:10 +0100

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -27,4 +27,21 @@ function init()
   rotate() ;
 }
 
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read about the challenge";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+
 document.addEventListener("DOMContentLoaded",init,false);
